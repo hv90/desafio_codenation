9ff12facda51166d2e2ee5f6895d5c0aee8ddfcb
Hugo <hugomachado182@gmail.com>
2019-07-21 13:35:59 -0300

    Etapa 3 unida à etapa 2 por simplicidade

--- etapa2.js
+++ etapa2.js
@@ -6,19 +6,24 @@ atualizar o arquivo JSON, no campo decifrado.
 */
 
 
+
 const fs = require('fs');
 const fileName = './answer.json';
 const file = require(fileName);
+const crypto = require('crypto');
+const restler = require('restler');
+
 
 
 //lendo o arquivo criado
-fs.readFile(fileName, (err, data) => {
+ fs.readFile(fileName, (err, data) => {
     if (err) throw err;
 
     let answer = JSON.parse(data);
     let num_casas = answer.numero_casas;
     let cifrado = answer.cifrado;
     let decifrado = '';
+    let hash = '';
 
     cifrado = cifrado.toLowerCase();//conforme definido pelas regras
 
@@ -44,10 +49,13 @@ fs.readFile(fileName, (err, data) => {
         }
     }
 
+    //criando o resumo criptográfico
+    hash = crypto.createHash('sha1').update(decifrado, 'utf-8').digest('hex');
 
     //preparando a sobrescrita do arquivo json
     file.cifrado = cifrado;//garante que cifrado se não era minúsculo agora passa a ser
     file.decifrado = decifrado;
+    file.resumo_criptografico = hash;
 
     //escrevendo no arquivo json
     fs.writeFile(fileName, JSON.stringify(file, null, 2), (err) => {
@@ -55,4 +63,26 @@ fs.readFile(fileName, (err, data) => {
             throw err;
     })
 
-});
+    
+
+    //fazendo o post do json
+     restler.post('http://localhost:4000,
+                {
+                    multipart: true,
+                    file: 'answer',
+                    data: {
+                        'numero_casas': file.numero_casas,
+                        'token': file.token,
+                        'cifrado': file.cifrado,
+                        'decifrado': file.decifrado,
+                        'resumo_criptografico': file.resumo_criptografico
+                    }
+                }).on('complete', (data, response)=>{
+                    console.log(data.message + "\n"+response.statusMessage);
+                }) 
+
+}); 
+
+
+
+
